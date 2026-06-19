857178997860b11790855b5dd88759e88c4194c7
symbiotech <symbiotech3@gmail.com>
2026-06-19 17:03:25 +0200

    log why it failed

--- bonuscard_odoo/static/src/app/bonuscard_partner_line_patch.js
+++ bonuscard_odoo/static/src/app/bonuscard_partner_line_patch.js
@@ -45,7 +45,14 @@ patch(PartnerList.prototype, {
             partner.bonuscard_recruitment_code = statusResult.recruitment_code;
             partner.bonuscard_last_lookup_note = statusResult.note;
         } catch (error) {
-            this.notification.add(_t("Bonuscard registration failed."), { type: "danger", sticky: false });
+            this.notification.add(error.message || _t("Bonuscard registration failed."), { type: "danger", sticky: false });
+            logPosMessage(
+                "Bonuscard",
+                "registerPartnerToBonuscard",
+                "Bonuscard registration failed.",
+                false,
+                [{ partnerId: partner?.id, error }]
+            );
         }
     },
 });
