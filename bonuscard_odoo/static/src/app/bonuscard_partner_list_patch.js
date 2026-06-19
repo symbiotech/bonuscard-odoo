a8a557ae101f549e3d98f3b539e13805e1d7149b
symbiotech <symbiotech3@gmail.com>
2026-06-19 12:34:24 +0200

    refactor: remove bonuscard partner list patch file

--- bonuscard_odoo/static/src/app/bonuscard_partner_list_patch.js
+++
@@ -1,5 +0,0 @@
-/** @odoo-module */
-
-import { PartnerLine } from "@point_of_sale/app/screens/partner_list/partner_line/partner_line";
-
-PartnerLine.props = [...PartnerLine.props, "onClickRegisterBonuscard"];
