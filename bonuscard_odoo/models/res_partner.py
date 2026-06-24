f78b427afd68b1d0aa8c8a3aa15a7f7c8e0bd261
symbiotech <symbiotech3@gmail.com>
2026-06-24 18:41:19 +0200

    typo

--- bonuscard_odoo/models/res_partner.py
+++ bonuscard_odoo/models/res_partner.py
@@ -103,7 +103,9 @@ class ResPartner(models.Model):
     def _sync_bonuscard_status(self, raise_if_missing_instance=False):
         self.ensure_one()
         service = self.env["bonuscard.api.service"]
-company = (self.company_id or self.commercial_partner_id.company_id or self.env.company)
+        company = (
+            self.company_id or self.commercial_partner_id.company_id or self.env.company
+        )
         instance = service._get_company_instance(company)
         if not instance:
             if raise_if_missing_instance:
@@ -175,7 +177,9 @@ company = (self.company_id or self.commercial_partner_id.company_id or self.env.
     def action_register_to_bonuscard(self):
         self.ensure_one()
         service = self.env["bonuscard.api.service"]
-company = (self.company_id or self.commercial_partner_id.company_id or self.env.company)
+        company = (
+            self.company_id or self.commercial_partner_id.company_id or self.env.company
+        )
         instance = service._get_company_instance(company)
         if not instance:
             raise UserError(
